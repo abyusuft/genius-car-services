b44c7baa3b20da200dc85c8d6ef7568c2553bb7d
abyusuft <imabuyusuf@gmail.com>
2022-04-18 04:48:54 +0600

    Login and Register added

--- src/Pages/Register/Registar.js
+++ src/Pages/Register/Registar.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css'
 
 const Registar = () => {
+    const handleRegister = (event) => {
+        event.preventDefault();
+    }
     return (
         <div>
-            <h2>Please Registar</h2>
-            <form >
+            <h2 className='text-center text-primary'>Please Registar</h2>
+            <form onSubmit={handleRegister} className='register-form'>
                 <input type="text" name="name" id="" required placeholder='Your Name' />
                 <input type="email" name="email" id="" required placeholder='Your Email' />
                 <input type="password" name="password" id="" required placeholder='Your Password' />
-                <input type="submit" value="Register" />
+                <input className='btn-primary' type="submit" value="Register" />
             </form>
+            <p className='text-center'>Already have an account? <Link className='text-danger text-decoration-none' to={'/login'} > Please Login!</Link></p>
         </div>
     );
 };
